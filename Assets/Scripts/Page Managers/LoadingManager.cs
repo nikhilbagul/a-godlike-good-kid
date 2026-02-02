e847f02bb04df049e164abcd14cfca1679e13b7a
nikhilbagul <nik2522@gmail.com>
2026-02-02 13:05:26 -0800

    Disable telemetry and debugger
    - Update project's bundle ID and build provisioning for updated iOS builds

---
+++ Assets/Scripts/Page Managers/LoadingManager.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class LoadingManager : MonoBehaviour
+{
+
+    public CanvasGroup loadingScreenCanvas;
+
+    private string fileName = "data.sav";
+    string path = "";
+
+    GameObject loadButton;
+    void Start()
+    {
+        loadButton = GameObject.Find("load game button");
+        loadButton.GetComponent<Button>().interactable = false;
+
+        //get current guid
+        SaveData data = Persistor.Load();
+
+
+#if !UNITY_EDITOR
+        path = Application.persistentDataPath + "/";
+        //userDataPath = Application.persistentDataPath + "/";
+#endif
+        path += fileName;
+        if (File.Exists(path) && Persistor.Load().levelName != "NULL")
+            loadButton.GetComponent<Button>().interactable = true;
+
+        StartCoroutine(DelayedShowCanvas());
+    } 
+
+    IEnumerator DelayedShowCanvas()
+    {
+        loadingScreenCanvas.alpha = 0;
+        float elapsedTime = 0;
+        while (elapsedTime < 1)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        elapsedTime = 0;
+        while (elapsedTime < 1)
+        {
+            elapsedTime += Time.deltaTime;
+            loadingScreenCanvas.alpha += Time.deltaTime;
+            yield return null;
+        }
+        loadingScreenCanvas.alpha = 1;
+    }
+
+   public void LoadFromCheckpoint()
+    {
+        GameManager.Instance.UnsetCamera();
+        CheckpointManager.LoadSceneFromCheckpoint();
+    }
+
+   public void NewGame()
+    {
+        //send data
+        //DataPoster.instance.SendUserData(new_game_url + GUID + replay_times.ToString() + System.DateTime.Now.ToString());
+        GMPersistor.Instance.SetupGUID();
+
+#if UNITY_EDITOR
+        URLSetter.NewGameCount(true);
+#else
+        URLSetter.NewGameCount();
+#endif
+
+        StartCoroutine(DelayedStart());
+    }
+
+    IEnumerator DelayedStart()
+    {
+        GameObject.Find("New game button").GetComponent<Button>().interactable = false;
+        yield return new WaitForSeconds(0.5f);
+        CheckpointManager.NewGame();
+    }
+}
